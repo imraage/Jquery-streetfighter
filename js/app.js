1d11aed597e6a40ec85ab76e9ed4ae81866c6580
imraage <raageo@gmail.com>
2014-03-23 23:04:06 -0500

    Ryu final

--- js/app.js
+++ js/app.js
@@ -23,10 +23,22 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
   	$('.ryu-ready').show();
   });
-})
+});
+$(document).keydown(function(event){
+		if (event.keyCode == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
+	});
+$(document).keyup(function(event) {
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+	});
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
-}
+};
